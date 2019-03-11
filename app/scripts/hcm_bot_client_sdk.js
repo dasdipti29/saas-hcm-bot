b3c4cd69de0318c3fb99400cb9dd4b8aadbc78f0
dipti <das.dipti29@gmail.com>
2019-03-11 15:06:44 +0530

    Update hcm_bot_client_sdk.js

--- app/scripts/hcm_bot_client_sdk.js
+++ app/scripts/hcm_bot_client_sdk.js
@@ -1,7 +1,6 @@
   !function(e,n,t,r){
   function o(){try{var e;if((e="string"==typeof this.response?JSON.parse(this.response):this.response).url){var t=n.getElementsByTagName("script")[0],r=n.createElement("script");r.async=!0,r.src=e.url,t.parentNode.insertBefore(r,t)}}catch(e){}}var s,p,a,i=[],c=[];e[t]={init:function(){s=arguments;var e={then:function(n){return c.push({type:"t",next:n}),e},catch:function(n){return c.push({type:"c",next:n}),e}};return e},on:function(){i.push(arguments)},render:function(){p=arguments},destroy:function(){a=arguments}},        e.__onWebMessengerHostReady__=function(n){if(delete e.__onWebMessengerHostReady__,e[t]=n,s)for(var r=n.init.apply(n,s),o=0;o<c.length;o++){var u=c[o];r="t"===u.type?r.then(u.next):r.catch(u.next)}p&&n.render.apply(n,p),a&&n.destroy.apply(n,a);for(o=0;o<i.length;o++)n.on.apply(n,i[o])};var u=new XMLHttpRequest;u.addEventListener("load",o), u.addEventListener("load", s), u.open("GET", r+"/loader.json", !0),u.responseType = "json", u.send()
-    }(window, document, "Bots", "https://saas-hcm-bot.herokuapp.com/client_sdk_js");
-
+    }(window, document, "Bots", "https://saas-hcm-bot.herokuapp.com/scripts");
 var access_token=null;
 var user_id=null;
 var Servlet_uri = "https://"+window.location.host+"/fscmRestApi/tokenrelay";
